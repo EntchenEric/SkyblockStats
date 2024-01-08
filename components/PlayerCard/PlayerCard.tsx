30fa68b435ee2c1793760c4c9f245c9d783036dd
EntchenEric <eseidel2004@gmail.com>
2024-01-08 20:17:10 +0100

    Refactor PlayerCard and PlayerCardGrid components

--- components/PlayerCard/PlayerCard.tsx
+++ components/PlayerCard/PlayerCard.tsx
@@ -1,25 +1,35 @@
 import { Card, Image, Text, Badge, Button, Group, Container } from '@mantine/core';
+import styled from '@emotion/styled';
 
+const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;  // Aligns items vertically in the center
+`;
+
+const StyledTextContainer = styled.div`
+  margin-left: 16px;  // Adds some space between the image and the text
+`;
 
 export function PlayerCard({ name, badge }: { name: string, badge: string }) {
   return (
     <Container>
-      <div>
-
+      <StyledDiv>
         <Image
           src={`https://mc-heads.net/avatar/${name}`}
-          height={160}
-          alt="Norway"
+          height={100}
+          alt="Player Avatar"
         />
-        <Group justify="space-between" mt="md" mb="xs">
-          <Text fw={500}>{name}</Text>
-        </Group>
-        {
-          badge
-            ? <Group><Badge color="pink">{badge}</Badge></Group>
-            : <></>
-        }
-      </div>
+        <StyledTextContainer>
+          <Group>
+            <Text fw={500}>{name}</Text>
+          </Group>
+          {
+            badge
+              ? <Group><Badge color="pink">{badge}</Badge></Group>
+              : null  // Using null is cleaner than <></>
+          }
+        </StyledTextContainer>
+      </StyledDiv>
     </Container>
   );
 }
--- components/PlayerCardGrid/PlayerCardGrid.tsx
+++ components/PlayerCardGrid/PlayerCardGrid.tsx
@@ -10,7 +10,7 @@ export function PlayerCardGrid({ players }: { players: Array<{ name: string, bad
 
     return (
         <Container>
-            <SimpleGrid cols={{ base: 3, sm: 5, lg: 5 }}>
+            <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }}>
                 {PlayerList}
             </SimpleGrid>
         </Container>
