dc4e7170492c25bd8f99a07444db8a6ad93d5fa9
elwood24 <dennis.matthiesen2003@gmail.com>
2024-01-08 20:10:04 +0100

    Update SearchBar component to use TextInput and add validation for playername input.

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,16 +1,27 @@
-import { Input } from '@mantine/core';
+import { TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { getUUIDFromName } from '../../api/getUUIDFromName';
 
 export function SearchBar() {
   const form = useForm({
     initialValues: {
       playername: '',
     },
+    validate: {
+      playername: (value) =>
+        value.length <= 1
+          ? 'Playername has to be longer'
+          : value.length >= 32
+            ? 'Playername has to be shorter'
+            : getUUIDFromName(value) === null
+              ? "Playername doesn't exist"
+              : null,
+    },
   });
 
   return (
     <form onSubmit={form.onSubmit((values) => console.log(values))}>
-      <Input placeholder="Enter Playername" {...form.getInputProps('playername')} />
+      <TextInput placeholder="Enter Playername" {...form.getInputProps('playername')} />
     </form>
   );
 }
