c7eaa938e3a7025fdeac33531ee2bb1bc63458a0
elwood24 <dennis.matthiesen2003@gmail.com>
2024-01-08 22:29:42 +0100

    Update PlayerPets component to display player's pets

--- components/PlayerPets/PlayerPets.tsx
+++ components/PlayerPets/PlayerPets.tsx
@@ -1,6 +1,34 @@
-import { useState } from 'react';
+import { Container, SimpleGrid } from '@mantine/core';
+import { useState, useEffect } from 'react';
 
-export function PlayerPets(profileData: any) {
-  console.log(profileData);
-  return <></>;
+interface PetDataInterface {
+  active: boolean;
+  candyUsed: number;
+  exp: number;
+  heldItem: string;
+  skin: string | null;
+  tier: 'COMMON' | 'UNCOMMON' | 'RARE' | 'EPIC' | 'LEGENDARY' | 'MYTHIC';
+  type: string;
+  uniqueId: string;
+  uuid: string | null;
+}
+
+export function PlayerPets({ profileData, uuid }: { profileData: any; uuid: string }) {
+  const [petData, setPetData] = useState<Array<PetDataInterface>>(
+    profileData.members[uuid.replaceAll('-', '')].pets_data.pets
+  );
+
+  const petDatas = petData.map((pet) => {
+    return <Container mt={15}>{pet.type}</Container>;
+  });
+
+  return (
+    <>
+      {petData == undefined ? (
+        <div>loading</div>
+      ) : (
+        <SimpleGrid cols={{ base: 1, sm: 4, lg: 7 }}>{petDatas}</SimpleGrid>
+      )}
+    </>
+  );
 }
