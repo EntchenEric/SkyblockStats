e6c96bc8bf563d1a9e3d8df39ea2976563759379
EntchenEric <eseidel2004@gmail.com>
2024-01-08 22:29:01 +0100

    Tryed to decode Inventory :(

--- app/[...playername]/page.tsx
+++ app/[...playername]/page.tsx
@@ -8,48 +8,48 @@ import { PlayerInventory } from '../../components/PlayerInventory/PlayerInventor
 import { PlayerPets } from '../../components/PlayerPets/PlayerPets';
 
 export default function player({ params }: any) {
-  const [uuid, setUUID] = useState();
-  const [profileData, setProfileData] = useState();
-  const [selectedProfile, setSelectedProfile] = useState();
-  const fetchData = async () => {
-    const uuid = await getUUIDFromName(params.playername);
-    setUUID(uuid);
-    const profileData = (await getSkyblockProfileData(uuid)).profiles;
-    setProfileData(profileData);
-    profileData.forEach((profile: any) => {
-      if (profile && profile.selected) {
-        setSelectedProfile(profile);
-      }
-    });
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return (
-    <>
-      <Container size={'lg'}>
-        <SimpleGrid cols={{ base: 3, sm: 6, lg: 10 }} mt={20}>
-          <Button p={0}>Inventory</Button>
-          <Button p={0}>Enderchest</Button>
-          <Button p={0}>Backpacks</Button>
-          <Button p={0}>Wardrobe</Button>
-          <Button p={0}>Pets</Button>
-          <Button p={0}>Accessories</Button>
-          <Button p={0}>Progression</Button>
-          <Button p={0}>Collection</Button>
-          <Button p={0}>Minions</Button>
-          <Button p={0}>Misc</Button>
-        </SimpleGrid>
-      </Container>
-
-            <Container size={"lg"}> 
-            {
-                uuid && selectedProfile
-                ? <PlayerInventory profileData={selectedProfile} uuid = {uuid} />
-                : "Loading..."
+    const [uuid, setUUID] = useState();
+    const [profileData, setProfileData] = useState();
+    const [selectedProfile, setSelectedProfile] = useState();
+    const fetchData = async () => {
+        const uuid = await getUUIDFromName(params.playername);
+        setUUID(uuid);
+        const profileData = (await getSkyblockProfileData(uuid)).profiles;
+        setProfileData(profileData);
+        profileData.forEach((profile: any) => {
+            if (profile && profile.selected) {
+                setSelectedProfile(profile);
             }
+        });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <Container size={'lg'}>
+                <SimpleGrid cols={{ base: 3, sm: 6, lg: 10 }} mt={20}>
+                    <Button p={0}>Inventory</Button>
+                    <Button p={0}>Enderchest</Button>
+                    <Button p={0}>Backpacks</Button>
+                    <Button p={0}>Wardrobe</Button>
+                    <Button p={0}>Pets</Button>
+                    <Button p={0}>Accessories</Button>
+                    <Button p={0}>Progression</Button>
+                    <Button p={0}>Collection</Button>
+                    <Button p={0}>Minions</Button>
+                    <Button p={0}>Misc</Button>
+                </SimpleGrid>
+            </Container>
+
+            <Container size={"lg"}>
+                {
+                    uuid && selectedProfile
+                        ? <PlayerInventory profileData={selectedProfile} uuid={uuid} />
+                        : "Loading..."
+                }
             </Container>
         </>
     )
